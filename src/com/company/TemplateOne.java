5bd1218c89c71df600a427f43ec6f40ee10852bc
anisawalji <70586927+anisawalji@users.noreply.github.com>
2020-09-04 09:31:39 -0500

    Initial commit

---
+++ src/com/company/TemplateOne.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class TemplateOne {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World!");
+        System.out.print("Goodbye World!");
+    }
+}
